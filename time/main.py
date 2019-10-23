e3c06977e3dd76e730e7c352f46d19d37e4756ed
serpasj@gmail.com <serpasj@gmail.com>
2019-10-22 20:14:32 -0700

    time module

---
+++ time/main.py
@@ -0,0 +1,16 @@
+import datetime
+import os
+import requests
+from flask import Flask
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def epoch_time():
+    return str(datetime.datetime.now().timestamp())
+
+
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 6378))
+    app.run(host="0.0.0.0", port=port, debug=True)
