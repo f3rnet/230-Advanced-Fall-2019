f107778cf86d3799b0a998a79ed5b392051c8890
serpasj@gmail.com <serpasj@gmail.com>
2019-11-24 21:06:28 -0800

    production settings

---
+++ mysite-3/blogging/admin.py
@@ -0,0 +1,21 @@
+from django.contrib import admin
+from blogging.models import Post, Category
+# Register your models here.
+# and a new admin registration
+
+
+class CategoryInline(admin.TabularInline):
+    model = Category.posts.through
+# @admin.register(Post)
+
+
+class PostAdmin(admin.ModelAdmin):
+    inlines = [CategoryInline]
+# @admin.register(Category)
+
+
+class CategoryAdmin(admin.ModelAdmin):
+    exclude = ('posts',)
+
+admin.site.register(Post, PostAdmin)
+admin.site.register(Category, CategoryAdmin)
---
+++ mysite-3/blogging/views.py
@@ -0,0 +1,81 @@
+from django.shortcuts import render
+from django.contrib.auth.models import User, Group
+from django.http import HttpResponse, HttpResponseRedirect, Http404
+from django.template import loader
+from blogging.models import Post, Category
+from rest_framework import viewsets
+from blogging.serializers import UserSerializer, GroupSerializer, PostSerializer, CategorySerializer
+
+
+class UserViewSet(viewsets.ModelViewSet):
+    """
+    API endpoint that allows users to be viewed or edited.
+    """
+    queryset = User.objects.all().order_by('-date_joined')
+    serializer_class = UserSerializer
+
+
+class GroupViewSet(viewsets.ModelViewSet):
+    """
+    API endpoint that allows groups to be viewed or edited.
+    """
+    queryset = Group.objects.all()
+    serializer_class = GroupSerializer
+
+
+class PostViewSet(viewsets.ModelViewSet):
+    """
+    API endpoint that allows posts to be viewed or edited.
+    """
+    queryset = Post.objects.all()
+    serializer_class = PostSerializer
+
+
+class CategoryViewSet(viewsets.ModelViewSet):
+    """
+    API endpoint that allows categories to be viewed or edited.
+    """
+    queryset = Category.objects.all()
+    serializer_class = CategorySerializer
+
+
+def stub_view(request, *args, **kwargs):
+    body = "Stub View\n\n"
+    if args:
+        body += "Args:\n"
+        body += "\n".join(["\t%s" % a for a in args])
+    if kwargs:
+        body += "Kwargs:\n"
+        body += "\n".join(["\t%s: %s" % i for i in kwargs.items()])
+    return HttpResponse(body, content_type="text/plain")
+
+def list_view(request):
+    published = Post.objects.exclude(published_date__exact=None)
+    posts = published.order_by('-published_date')
+    context = {'posts': posts}
+    return render(request, 'blogging/list.html', context)
+
+def detail_view(request, post_id):
+    published = Post.objects.exclude(published_date__exact=None)
+    try:
+        post = published.get(pk=post_id)
+    except Post.DoesNotExist:
+        raise Http404
+    context = {'post': post}
+    return render(request, 'blogging/detail.html', context)
+
+
+def logout(request):
+    context = {
+        'posts': Post.objects.order_by('-date')
+        if request.user.is_authenticated else []
+    }
+    return render(request, 'logout/index.html', context)
+
+def index(request):
+    context = {
+        'posts': Post.objects.order_by('-date')
+        if request.user.is_authenticated else []
+    }
+    return render(request, 'blog/index.html', context)
+
